ac527e0f5bf2b98784600f543500bd8e4efc8fa1
Parshab <Parshab@gmail.com>
2025-02-18 09:10:32 +0545

    added

--- parshab/myurls.py
+++ parshab/myurls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('homee/',homee),
     path('task/',task),
     path('form/',form),
+    path('task/<pk>/',mark),   # for every task, we need to create the new url and new html page.
+    path('task/<pk>/edit',edit_task),
+    path('task/<pk>/delete',delete_task),
 ]
--- parshab/views.py
+++ parshab/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render , HttpResponse , redirect
 from .models import *
 # Create your views here.
 def homee(request):
@@ -45,5 +45,51 @@ def task(request):
 def form(request):
     if request.method == 'POST':
         title = request.POST.get('title')
-        descriptions = request.POST.get('descriptions')
-        task = Todolist(title=title,descriptions=descriptions)
+        description = request.POST.get('description')
+        if title == '' or description == '':
+            context = {
+                'error':'Please fill both Fields.'
+            }
+            return render(request,'form.html',context)
+        else:
+            Todolist.objects.create(title = title,description=description)
+            return redirect('form/')
+    return render(request,'form.html')
+
+
+def mark(request,pk):
+    task = Todolist.objects.get(pk = pk)
+    task.is_completed = False
+    task.save()
+    return redirect('/')
+
+def edit_task(request,pk):
+    task = Todolist.objects.get(pk=pk)
+    context = {
+        "tasks":task
+    }
+    if request.method == 'POST':
+        # getting the data from the form
+        titles = request.POST.get('title')
+        descriptions = request.POST.get('description')
+        if titles == '' or descriptions == '':
+            context = {
+                'error':'Please fill both Fields.'
+            }
+            return render(request,'edit.html',context)
+            # Now editing the task
+        task.title = titles
+        task.description = descriptions
+        task.save()
+        return redirect("task/")
+    return render(request,'edit.html',context)
+    
+def delete_task(request,pk):
+    task = Todolist.objects.get(pk = pk)
+    context = {'tasks':task}
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    return render(request,'delete.html',context)
+
+    
