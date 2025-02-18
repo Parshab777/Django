da919dadc73d720e1c00972158cc003113106736
Parshab <Parshab@gmail.com>
2025-02-18 20:06:20 +0545

    Add

--- parshab/admin.py
+++ parshab/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(Todolist)
+class PTodolistAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'is_completed']
+    list_editable = ["is_completed"]
--- parshab/models.py
+++ parshab/models.py
@@ -6,3 +6,8 @@ class Todolist(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
     is_completed=models.BooleanField(default = False)
+
+def __str__(self):
+    return self.title
+    
+    
